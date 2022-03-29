64092b5a4fa11c0649fc99861298dca4c9b76920
Charlie-Junhyung <june900s@gmail.com>
2022-03-29 17:29:51 +0900

    A2HS

--- js/main.js
+++ js/main.js
@@ -2,7 +2,35 @@ window.onload = () => {
     'use strict';
   
     if ('serviceWorker' in navigator) {
-      navigator.serviceWorker
-               .register('./sw.js');
+      navigator.serviceWorker.register('./sw.js').then( () => {
+        console.info('Service Worker Registered');
+      });
     }
+
+    let deferredPrompt;
+    const addButton = document.querySelector('#buttonInstall');
+    addButton.style.display ='none';
+    
+    window.addEventListener('beforeinstallprompt', e => {
+      e.preventDefault(); // Prevent Chrome 67 and earlier from automatically showing the prompt
+      deferredPrompt = e;
+      // Update UI to notify the user they can add to home screen 
+      addButton.style.display = 'block';
+
+      addButton.addEventListener('click', () => {
+        addButton.style.display = 'none'; // hide our user interface that shows our A2HS button
+        deferredPrompt.prompt();
+
+        // Wait for the user to respond to the prompt
+        deferredPrompt.userChoice.then(choiceResult => {
+          if (choiceResult.outcome === 'accepted')
+            console.info('User accepted the A2HS prompt');
+          else 
+            console.info('User dismissed the A2HS prompt');
+
+          deferredPrompt = null;
+        });
+
+      });
+    });
 }
